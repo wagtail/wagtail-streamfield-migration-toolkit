dfc301f69952385b595f612f05dfa6cb18c8cdc2
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-06-24 13:09:30 +0530

    empty function that can be imported to the tests

--- wagtail_streamfield_migration_toolkit/utils.py
+++ wagtail_streamfield_migration_toolkit/utils.py
@@ -1,3 +1,6 @@
 
-def hello_world():
-    print("Hello World")
+
+def apply_changes_to_raw_data(raw_data, block_path, operation_label, *args, **kwargs):
+    altered_raw_data = raw_data
+    # Logic will be here 
+    return altered_raw_data
