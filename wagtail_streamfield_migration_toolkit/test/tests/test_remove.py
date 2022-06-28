e7b502f174da476e59cdb2d88a01348d86bba978
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-06-28 14:37:08 +0530

    added more test cases wrote some of the tests with factories  small changes

--- wagtail_streamfield_migration_toolkit/test/tests/test_remove.py
+++ wagtail_streamfield_migration_toolkit/test/tests/test_remove.py
@@ -2,23 +2,28 @@ from unittest import expectedFailure
 from django.test import SimpleTestCase, TestCase
 
 from wagtail_streamfield_migration_toolkit.utils import apply_changes_to_raw_data
+from .. import factories
 
 
 class RemoveUtilsTestCase(SimpleTestCase):
     pass
 
 
-class RemoveRawDataIndividualTestCase(SimpleTestCase):
+class RemoveRawDataIndividualTestCase(TestCase):
     """
-    Tests with raw json data for different possible block structures involved in renaming.
-    Each test here only includes just the
+    Tests with raw json data for different possible block structures involved in removing.
+    Each test here only includes just an instance of the block which is being removed.
     """
+
+    # TODO complete rest with wagtail-factories when available
 
     @expectedFailure
     def test_simple_remove(self):
         """Remove `char1`"""
 
-        raw_data = [{"type": "char1", "value": "Char Block 1"}]
+        raw_data = factories.SampleModelFactory(
+            content__0__char1__value="Char Block 1"
+        ).content.raw_data
         altered_raw_data = apply_changes_to_raw_data(raw_data, "char1", "remove")
 
         self.assertEqual(len(altered_raw_data), 0)
@@ -27,24 +32,21 @@ class RemoveRawDataIndividualTestCase(SimpleTestCase):
     def test_struct_nested_remove(self):
         """Remove `simplestruct.char1`"""
 
-        raw_data = [
-            {
-                "type": "simplestruct",
-                "value": {
-                    "char1": "Char Block 1",
-                    "char2": "Char Block 2",
-                },
-            }
-        ]
+        raw_data = factories.SampleModelFactory(
+            content__0__simplestruct__label="SimpleStruct 1"
+        ).content.raw_data
         altered_raw_data = apply_changes_to_raw_data(
             raw_data, "simplestruct.char1", "remove"
         )
 
+        self.assertEqual(len(altered_raw_data[0]["value"]), 1)
         self.assertNotIn("char1", altered_raw_data[0]["value"])
 
     @expectedFailure
     def test_stream_nested_remove(self):
         """Remove `simplestream.char1`"""
+
+        # TODO use factories when available
 
         raw_data = [
             {
@@ -62,6 +64,8 @@ class RemoveRawDataIndividualTestCase(SimpleTestCase):
     def test_struct_stream_nested_remove(self):
         """Remove `nestedstruct.stream1.char1`"""
 
+        # TODO use factories when available
+
         raw_data = [
             {
                 "type": "nestedstruct",
@@ -72,16 +76,18 @@ class RemoveRawDataIndividualTestCase(SimpleTestCase):
             raw_data, "nestedstruct.stream1.char1", "remove"
         )
 
-        nested_block = altered_raw_data[0]["value"]
-        self.assertEqual(len(nested_block["stream1"]), 0)
+        nested_block = altered_raw_data[0]["value"]["stream1"]
+        self.assertEqual(len(nested_block), 0)
 
     @expectedFailure
     def test_list_stream_nested_remove(self):
-        """Remove `nestedlist.char1`"""
+        """Remove `nestedlist_stream.char1`"""
+
+        # TODO use factories when available
 
         raw_data = [
             {
-                "type": "nestedlist",
+                "type": "nestedlist_stream",
                 "value": [
                     {
                         "type": "item",
@@ -91,18 +97,96 @@ class RemoveRawDataIndividualTestCase(SimpleTestCase):
             }
         ]
         altered_raw_data = apply_changes_to_raw_data(
-            raw_data, "nestedlist.char1", "remove"
+            raw_data, "nestedlist_stream.char1", "remove"
         )
 
         nested_block = altered_raw_data[0]["value"]
         self.assertEqual(len(nested_block[0]["value"]), 0)
 
+    @expectedFailure
+    def test_list_struct_nested_remove(self):
+        "Remove `nestedlist_struct.char1`"
 
-class RemoveRawDataFullTestCase(TestCase):
-    # TODO check wagtail_factories
+        raw_data = factories.SampleModelFactory(
+            content__0__nestedlist_struct__0__label="NestedList Struct 1"
+        ).content.raw_data
+        altered_raw_data = apply_changes_to_raw_data(
+            raw_data, "nestedlist_struct.char1", "remove"
+        )
+
+        nested_block = altered_raw_data[0]["value"][0]
+        self.assertEqual(len(nested_block["value"]), 1)
+
+
+class RemoveRawDataMultipleTestCase(TestCase):
     # TODO test multiple blocks at once
     # TODO test other blocks intact
-    pass
+
+    @expectedFailure
+    def test_simple_remove(self):
+        """Remove `char1`"""
+
+        raw_data = factories.SampleModelFactory(
+            content__0__char1__value="Char Block 1",
+            content__1__char2__value="Char Block 2",
+            content__2__char1__value="Char Block 1",
+        ).content.raw_data
+        
+        altered_raw_data = apply_changes_to_raw_data(raw_data, "char1", "remove")
+
+        self.assertEqual(len(altered_raw_data), 1)
+        self.assertEqual(altered_raw_data[0]["type"], "char2")
+
+    @expectedFailure
+    def test_struct_nested_remove(self):
+        """Remove `simplestruct.char1`"""
+
+        raw_data = factories.SampleModelFactory(
+            content__0__char1__value="Char Block 1",
+            content__1__simplestruct__label="SimpleStruct 1",
+            content__2__simplestruct__label="SimpleStruct 2",
+        ).content.raw_data
+
+        altered_raw_data = apply_changes_to_raw_data(
+            raw_data, "simplestruct.char1", "remove"
+        )
+
+        self.assertEqual(len(altered_raw_data), 3)
+        self.assertEqual(len(altered_raw_data[1]["value"]), 1)
+        self.assertEqual(len(altered_raw_data[2]["value"]), 1)
+        self.assertNotIn("char1", altered_raw_data[1]["value"])
+        self.assertNotIn("char1", altered_raw_data[2]["value"])
+        self.assertIn("char2", altered_raw_data[1]["value"])
+        self.assertIn("char2", altered_raw_data[2]["value"])
+
+    @expectedFailure
+    def test_list_struct_nested_remove(self):
+        "Remove `nestedlist_struct.char1`"
+
+        raw_data = factories.SampleModelFactory(
+            content__0__char1__value="Char Block 1",
+            content__1__nestedlist_struct__0__label="NestedList Struct 1",
+            content__1__nestedlist_struct__1__label="NestedList Struct 2",
+            content__2__nestedlist_struct__0__label="NestedList Struct 3",
+        ).content.raw_data
+
+        altered_raw_data = apply_changes_to_raw_data(
+            raw_data, "nestedlist_struct.char1", "remove"
+        )
+
+        self.assertEqual(len(altered_raw_data), 3)
+        self.assertEqual(len(altered_raw_data[1]["value"]), 2)
+        self.assertEqual(len(altered_raw_data[2]["value"]), 1)
+        self.assertEqual(len(altered_raw_data[1]["value"][0]["value"]), 1)
+        self.assertEqual(len(altered_raw_data[1]["value"][1]["value"]), 1)
+        self.assertEqual(len(altered_raw_data[2]["value"][1]["value"]), 1)
+
+        self.assertNotIn("char1", altered_raw_data[1]["value"][0]["value"])
+        self.assertNotIn("char1", altered_raw_data[1]["value"][1]["value"])
+        self.assertNotIn("char1", altered_raw_data[2]["value"][0]["value"])
+        self.assertIn("char2", altered_raw_data[1]["value"][0]["value"])
+        self.assertIn("char2", altered_raw_data[1]["value"][1]["value"])
+        self.assertIn("char2", altered_raw_data[2]["value"][0]["value"])
 
 
 class RemoveTestCase(TestCase):
