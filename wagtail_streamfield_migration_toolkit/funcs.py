272f4da75750157c5cfaf796d352688fb39e6f8d
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-07-12 15:48:00 +0530

    Updating objects chunk by chunk - using `queryset.iterator()` - using `bulk_update` with a buffer - rename function as `migrate_stream_data`

--- wagtail_streamfield_migration_toolkit/funcs.py
+++ wagtail_streamfield_migration_toolkit/funcs.py
@@ -5,7 +5,7 @@ from wagtail.blocks import StreamValue
 from wagtail_streamfield_migration_toolkit import utils
 
 
-def stream_data_migration(
+def migrate_stream_data(
     apps,
     schema_editor,
     app_name,
@@ -14,12 +14,16 @@ def stream_data_migration(
     block_path_str,
     operation,
     with_revisions=False,
+    revision_limit=None,
+    chunk_size=1024,
 ):
     page_model = apps.get_model(app_name, model_name)
 
-    for page in page_model.objects.annotate(
+    page_queryset = page_model.objects.annotate(
         raw_content=Cast(F(field_name), JSONField())
-    ).all():
+    ).all()
+    updated_pages_buffer = []
+    for page in page_queryset.iterator(chunk_size=chunk_size):
 
         altered_raw_data = utils.apply_changes_to_raw_data(
             raw_data=page.raw_content,
@@ -27,17 +31,20 @@ def stream_data_migration(
             operation=operation,
             streamfield=getattr(page_model, field_name),
         )
+        # - TODO add a return value to util to know if changes were made - Where would this be added?
+        # - TODO save changed only
 
         stream_block = getattr(page, field_name).stream_block
         setattr(
             page, field_name, StreamValue(stream_block, altered_raw_data, is_lazy=True)
         )
+        updated_pages_buffer.append(page)
 
-        page.save()
+        if len(updated_pages_buffer) == chunk_size:
+            page_model.objects.bulk_update(updated_pages_buffer, [field_name])
+            updated_pages_buffer = []
 
-    # iterate over pages
-    # - rename_blocks_in_raw_content for each page
-    # - TODO add a return value to util to know if changes were made
-    # - TODO save changed only
-    # -
+    if len(updated_pages_buffer) > 0:
+        page_model.objects.bulk_update(updated_pages_buffer, [field_name])
+
     # TODO for revisions
