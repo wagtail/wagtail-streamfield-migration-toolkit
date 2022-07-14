8233ad81231878aa116f24dcfabeb05893edb234
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-07-14 11:09:50 +0530

    changes to iteration functions value passed instead of block operations take parent block

--- wagtail_streamfield_migration_toolkit/operations.py
+++ wagtail_streamfield_migration_toolkit/operations.py
@@ -2,7 +2,13 @@ class BaseBlockOperation:
     def __init__(self):
         pass
 
-    def apply_to_block(self, block):
+    def apply_to_stream_block_value(self, block_value, child_block_name):
+        raise NotImplementedError
+
+    def apply_to_list_block_value(self, block_value, child_block_name):
+        raise NotImplementedError
+
+    def apply_to_struct_block_value(self, block_value, child_block_name):
         raise NotImplementedError
 
 
@@ -11,13 +17,47 @@ class RenameBlockOperation(BaseBlockOperation):
         self.new_name = new_name
         super().__init__()
 
-    def apply_to_block(self, block):
-        return {**block, "type": self.new_name}
+    def apply_to_stream_block_value(self, block_value, child_block_name):
+        mapped_block_value = []
+        for child_block in block_value:
+            if child_block["type"] == child_block_name:
+                mapped_block_value.append({**child_block, "type": self.new_name})
+            else:
+                mapped_block_value.append(child_block)
+        return mapped_block_value
+
+    # Note that `apply_to_list_block_value` should not be called at all for this operation
+
+    def apply_to_struct_block_value(self, block_value, child_block_name):
+        mapped_block_value = {}
+        for key in block_value:
+            if key == child_block_name:
+                mapped_block_value[self.new_name] = block_value[key]
+            else:
+                mapped_block_value[key] = block_value[key]
+        return mapped_block_value
 
 
 class RemoveBlockOperation(BaseBlockOperation):
     def __init__(self):
         super().__init__()
 
-    def apply_to_block(self, block):
-        return None
+    def apply_to_stream_block_value(self, block_value, child_block_name):
+        mapped_block_value = []
+        for child_block in block_value:
+            if child_block["type"] == child_block_name:
+                continue
+            else:
+                mapped_block_value.append(child_block)
+        return mapped_block_value
+
+    # Note that `apply_to_list_block_value` should not be called at all for this operation
+
+    def apply_to_struct_block_value(self, block_value, child_block_name):
+        mapped_block_value = {}
+        for key in block_value:
+            if key == child_block_name:
+                continue
+            else:
+                mapped_block_value[key] = block_value[key]
+        return mapped_block_value
--- wagtail_streamfield_migration_toolkit/utils.py
+++ wagtail_streamfield_migration_toolkit/utils.py
@@ -6,158 +6,160 @@ from wagtail_streamfield_migration_toolkit.operations import BaseBlockOperation
 
 # TODO handle block_defs not existing? we'll do this later
 # TODO complete descriptions
-# TODO see if a BlockPath Class will be helpful for operations involving moving blocks
 
 
-def is_block_at_path_start(block, block_path):
-    return block["type"] == block_path[0]
+def is_block_at_path_start(block_name, block_path):
+    return block_name == block_path[0]
 
-def get_altered_or_unchanged_block(
-    block, block_def, block_path, operation: BaseBlockOperation, **kwargs
+
+def map_block_value(
+    block_value, block_def, block_path, operation: BaseBlockOperation, **kwargs
 ):
     # TODO complete description
 
-    # If the block is not at the start of `block_path`, then neither it nor it's children are
-    # blocks that we need to change.
-    if not is_block_at_path_start(block, block_path):
-        return block
-
     # If the `block_path` length is 1, that means we've reached the end of the block path, that
     # is, the block where we need to apply the operation.
-    elif len(block_path) == 1:
-        altered_block = operation.apply_to_block(block)
-        return altered_block
+    if len(block_path) == 1:
 
-    # Depending on whether the block is a ListBlock, StructBlock or StreamBlock we call a
+        # Since StreamBlocks, StructBlocks, and ListBlocks have their children in different
+        # formats, we have a different `apply_to_` operation for each. Note that for some
+        # operations all of these won't be used. For example rename operation won't use
+        # `apply_to_list_block_value`, since list blocks don't have any named direct children.
+        if isinstance(block_def, StreamBlock):
+            mapped_value = operation.apply_to_stream_block_value(
+                block_value, block_path[-1]
+            )
+        elif isinstance(block_def, ListBlock):
+            mapped_value = operation.apply_to_list_block_value(
+                block_value, block_path[-1]
+            )
+        elif isinstance(block_def, StructBlock):
+            mapped_value = operation.apply_to_struct_block_value(
+                block_value, block_path[-1]
+            )
+        else:
+            raise ValueError("Unexpected Structural Block + {}".format(block_value))
+
+        return mapped_value
+
+    # Again, depending on whether the block is a ListBlock, StructBlock or StreamBlock we call a
     # different function to alter its children.
 
-    if isinstance(block_def, ListBlock):
-        altered_block = {
-            **block,
-            "value": get_altered_or_unchanged_list_blocks(
-                block["value"],
-                operation=operation,
-                block_def=block_def,
-                block_path=block_path,
-                **kwargs
-            ),
-        }
-
-    elif isinstance(block_def, StructBlock):
-        altered_block = {
-            **block,
-            "value": get_altered_or_unchanged_struct_blocks(
-                block["value"],
-                operation=operation,
-                block_def=block_def,
-                block_path=block_path,
-                **kwargs
-            ),
-        }
-
-    elif isinstance(block_def, StreamBlock):
-        altered_block = {
-            **block,
-            "value": get_altered_or_unchanged_stream_blocks(
-                block["value"],
-                operation=operation,
-                block_def=block_def,
-                block_path=block_path,
-                **kwargs
-            ),
-        }
-
-    else:
-        # TODO figure out if this is needed and how to do it
-        raise ValueError("Unexpected Structural Block + {}".format(block))
-
-    return altered_block
-
-
-def get_altered_or_unchanged_struct_blocks(
-    struct_value, block_def, block_path, **kwargs
-):
-    altered_value = {}
-    for key in struct_value:
-        # we actually remove the key, value pair from the dict here
-        value = struct_value[key]
-        child_block_def = block_def.child_blocks[key]
-
-        # Here we pass the key, value pair in the same format as a normal block, that is,
-        # { type: key, value: value }
-        altered_block = get_altered_or_unchanged_block(
-            {"type": key, "value": value},
-            block_def=child_block_def,
-            block_path=block_path[1:],
+    if isinstance(block_def, StreamBlock):
+        mapped_value = map_stream_block_value(
+            block_value,
+            operation=operation,
+            block_def=block_def,
+            block_path=block_path,
             **kwargs
         )
 
-        # if the return value is none, then it's a remove operation and we don't need to add
-        # the key, value pair again. Otherwise, we add the returned blocks type and value as the
-        # new key, value pair to the dictionary
-        if altered_block is not None:
-            altered_value[altered_block["type"]] = altered_block["value"]
+    elif isinstance(block_def, ListBlock):
+        mapped_value = map_list_block_value(
+            block_value,
+            operation=operation,
+            block_def=block_def,
+            block_path=block_path,
+            **kwargs
+        )
+
+    elif isinstance(block_def, StructBlock):
+        mapped_value = map_struct_block_value(
+            block_value,
+            operation=operation,
+            block_def=block_def,
+            block_path=block_path,
+            **kwargs
+        )
+
+    else:
+        # TODO figure out if this is needed and how to do it
+        raise ValueError("Unexpected Structural Block + {}".format(block_value))
+
+    return mapped_value
+
+
+def map_stream_block_value(stream_block_value, block_def, block_path, **kwargs):
+
+    mapped_value = []
+    for child_block in stream_block_value:
+
+        # If the block is not at the start of `block_path`, then neither it nor it's children are
+        # blocks that we need to change.
+        if not is_block_at_path_start(child_block["type"], block_path):
+            mapped_value.append(child_block)
+
+        else:
+            child_block_def = block_def.child_blocks[child_block["type"]]
+            mapped_child_value = map_block_value(
+                child_block["value"],
+                block_def=child_block_def,
+                block_path=block_path[1:],
+                **kwargs
+            )
+            mapped_value.append({**child_block, "value": mapped_child_value})
+
+    return mapped_value
+
+
+def map_struct_block_value(struct_block_value, block_def, block_path, **kwargs):
+    altered_value = {}
+    for key in struct_block_value:
+        child_value = struct_block_value[key]
+
+        # If the block is not at the start of `block_path`, then neither it nor it's children are
+        # blocks that we need to change.
+        if not is_block_at_path_start(key, block_path):
+            altered_value[key] = child_value
+
+        else:
+            child_block_def = block_def.child_blocks[key]
+            altered_child_value = map_block_value(
+                child_value,
+                block_def=child_block_def,
+                block_path=block_path[1:],
+                **kwargs
+            )
+            altered_value[key] = altered_child_value
+
     return altered_value
 
 
 # TODO listblock item multiple ways, decide final approach
 # 1. user passes '...item...' in `block_path_str` itself
 #   - ad: can access listblock and immediate child separately for applying operations
-#           eg:- to change values. In this case user will have to use an operation which takes
-#           the parent listblocks values and updates all of them. Have to see if it's okay,
+#           eg:- to change values. If this is not the case user will have to use an operation which 
+#           takes the parent listblocks values and updates all of them. Have to see if it's okay,
 #           depending on how other operations have their `apply_to` operations; if it is only at
 #           block level then it is problematic: for example, if we're doing a value change and
 #           for stream and struct children it is applied directly to the child, while only for
-#           lists it is applied to the parent.
+#           lists it is applied to the parent. This may be even more problematic if there is a
+#           listblock inside a listblock
 #   - dis: this feels a bit ugly? since item isn't a name defined by the user, but internally
 #           added for listblocks.
-# 2. 'item' is automatically prepended to `block_path` in `get_altered_or_unchanged_list_blocks`
+# 2. 'item' is automatically prepended to `block_path` when we come across a list block
 #   - dis: user can't access listblock and immediate child separately, refer above
+#   - dis: this becomes a little messy with the current way we recurse through blocks, since we 
+#   -       can't do this just in one place (we will need to do this in the parent itself)
 # TODO if user passes 'item', update descriptions
-# Currently going with option 2
+# Currently going with option 1
 
 
-def get_altered_or_unchanged_list_blocks(blocks, block_def, block_path, **kwargs):
-    # At this point our current block path is like ['list1', 'nestedchild1', ...]
-    # However the actual blocks look like,
-    # { type: list1, values: [{ type: item, value: { nestedchild1: '', ... } }, ...] }
-    #
-    # Note that 'item' is not there in the block path. (Since `type: item` is an internal detail
-    # added in ListBlocks, not a name given by the user.)
-    #
-    # If we pass `block_path[1:]` now, it will look like ['nestedchild1', ...],
-    # where 'nestedchild1' is actually the child of the list item, not the immediate list child.
-    # To make up for this, we prepend 'item' to `block_path[1:]` so it will look like
-    # ['item', 'nestedchild1']
+def map_list_block_value(list_block_value, block_def, block_path, **kwargs):
 
-    altered_blocks = []
-    for block in blocks:
+    mapped_value = []
+    for child_block in list_block_value:
 
-        altered_or_unchanged_block = get_altered_or_unchanged_block(
-            block,
+        mapped_child_value = map_block_value(
+            child_block["value"],
             block_def=block_def.child_block,
-            block_path=["item"] + block_path[1:],
+            block_path=block_path[1:],
             **kwargs
         )
 
-        altered_blocks.append(altered_or_unchanged_block)
-    return altered_blocks
+        mapped_value.append({**child_block, "value": mapped_child_value})
 
-
-# TODO discuss better naming convention
-def get_altered_or_unchanged_stream_blocks(blocks, block_def, block_path, **kwargs):
-
-    altered_blocks = []
-    for block in blocks:
-        child_block_def = block_def.child_blocks[block["type"]]
-        altered_or_unchanged_block = get_altered_or_unchanged_block(
-            block, block_def=child_block_def, block_path=block_path[1:], **kwargs
-        )
-
-        # if the return value is None, then it's a removed block
-        if altered_or_unchanged_block is not None:
-            altered_blocks.append(altered_or_unchanged_block)
-    return altered_blocks
+    return mapped_value
 
 
 def apply_changes_to_raw_data(
@@ -170,14 +172,32 @@ def apply_changes_to_raw_data(
         raw_data:
             The current stream data (a list of top level blocks)
         block_path_str:
-            Currently `block_path_str` should be a '.' separated list of names of the blocks from
-            the top level block to the nested block which is subject to the operation.
+            Should be a '.' separated list of names of the blocks from the top level block to 
+            the nested block which is to be changed.
 
             eg:- 'simplestream.char1' would point to,
                 [..., { type: simplestream, value: [..., {type: char1, value: ''}] }]
+
+            NOTE: When the path contains a ListBlock child, 'item' must be added to the block as
+            the name of said child.
+
+            eg:- 'list1.item.char1' where the list child is a StructBlock would point to,
+                [
+                    ...,
+                    {
+                        type: list1,
+                        value: [
+                            {
+                                type: item,
+                                value: { ..., char1: "" }
+                            },
+                            ...
+                        ]
+                    }
+                ]
         operation:
-            A subclass of `operations.BaseBlockOperation`. It will have the `apply_to` method
-            for applying changes to a block.
+            A subclass of `operations.BaseBlockOperation`. It will have the `apply_to_` methods
+            for applying changes to the parent block of the block type that must be changed.
         streamfield:
             The streamfield for which data is being migrated. This is used to get the definitions
             of the blocks.
@@ -189,19 +209,13 @@ def apply_changes_to_raw_data(
     block_path = block_path_str.split(".")
     block_def = streamfield.field.stream_block
 
-    # TODO this might be better if we are moving top level blocks (there wouldn't be a common
-    # outermost block otherwise)
-    field_as_block = {}
-    field_as_block["type"] = ""
-    field_as_block["value"] = raw_data
-    block_path = [""] + block_path
-
-    altered_raw_data = get_altered_or_unchanged_block(
-        field_as_block,
+    # TODO map rename convention
+    altered_raw_data = map_block_value(
+        raw_data,
         block_def=block_def,
         block_path=block_path,
         operation=operation,
         **kwargs
     )
 
-    return altered_raw_data["value"]
+    return altered_raw_data
