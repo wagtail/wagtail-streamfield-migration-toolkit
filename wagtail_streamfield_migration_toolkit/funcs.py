0290ab8699bce3eaa75adf671e693696e50ac599
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-07-26 23:49:08 +0530

    subclass `RunPython` and allow multiple operations - replace `migrate_stream_data` func with `MigrateStreamData` class - subclasses RunPython and we define forward method here - take multiple operations instead of a single operation - get `operations_and_block_paths` as a list of tuples - limit revisions from a date instead of a number

--- wagtail_streamfield_migration_toolkit/funcs.py
+++ wagtail_streamfield_migration_toolkit/funcs.py
@@ -1,88 +1,108 @@
 import json
-from django.db.models import JSONField, F, Subquery, OuterRef
+from django.db.models import JSONField, F
 from django.db.models.functions import Cast
+from django.db.migrations import RunPython
 from wagtail.blocks import StreamValue
 
 from wagtail_streamfield_migration_toolkit import utils
 
 
-def migrate_stream_data(
-    apps,
-    schema_editor,
-    app_name,
-    model_name,
-    field_name,
-    block_path_str,
-    operation,
-    with_revisions=False,
-    revision_limit=None,
-    chunk_size=1024,
-):
-    page_model = apps.get_model(app_name, model_name)
+class MigrateStreamData(RunPython):
+    def __init__(
+        self,
+        app_name,
+        model_name,
+        field_name,
+        operations_and_block_paths,
+        revisions_from=None,
+        chunk_size=1024,
+        **kwargs
+    ):
+        # TODO add checks to validate
+        self.app_name = app_name
+        self.model_name = model_name
+        self.field_name = field_name
+        self.operations_and_block_paths = operations_and_block_paths
+        self.revisions_from = revisions_from
+        self.chunk_size = chunk_size
 
-    page_queryset = page_model.objects.annotate(
-        raw_content=Cast(F(field_name), JSONField())
-    ).all()
-    updated_pages_buffer = []
-    for page in page_queryset.iterator(chunk_size=chunk_size):
-
-        altered_raw_data = utils.apply_changes_to_raw_data(
-            raw_data=page.raw_content,
-            block_path_str=block_path_str,
-            operation=operation,
-            streamfield=getattr(page_model, field_name),
+        # TODO add reverse code when needed, will probably need another input (reversible?)
+        # super class kwargs - atomic,elidable,hints
+        super().__init__(
+            code=self.migrate_stream_data_forward,
+            reverse_code=lambda *args: None,
+            **kwargs
         )
-        # - TODO add a return value to util to know if changes were made - Where would this be added?
-        # - TODO save changed only
 
-        stream_block = getattr(page, field_name).stream_block
-        setattr(
-            page, field_name, StreamValue(stream_block, altered_raw_data, is_lazy=True)
-        )
-        updated_pages_buffer.append(page)
+    def migrate_stream_data_forward(self, apps, schema_editor):
+        page_model = apps.get_model(self.app_name, self.model_name)
 
-        if len(updated_pages_buffer) == chunk_size:
-            page_model.objects.bulk_update(updated_pages_buffer, [field_name])
-            updated_pages_buffer = []
+        page_queryset = page_model.objects.annotate(
+            raw_content=Cast(F(self.field_name), JSONField())
+        ).all()
+        updated_pages_buffer = []
+        for page in page_queryset.iterator(chunk_size=self.chunk_size):
 
-    if len(updated_pages_buffer) > 0:
-        page_model.objects.bulk_update(updated_pages_buffer, [field_name])
+            altered_raw_data = page.raw_content
+            for operation, block_path_str in self.operations_and_block_paths:
+                altered_raw_data = utils.apply_changes_to_raw_data(
+                    raw_data=altered_raw_data,
+                    block_path_str=block_path_str,
+                    operation=operation,
+                    streamfield=getattr(page, self.field_name),
+                )
+                # - TODO add a return value to util to know if changes were made - Where would this be added?
+                # - TODO save changed only
 
-    if with_revisions:
+            stream_block = getattr(page, self.field_name).stream_block
+            setattr(
+                page,
+                self.field_name,
+                StreamValue(stream_block, altered_raw_data, is_lazy=True),
+            )
+            updated_pages_buffer.append(page)
+
+            if len(updated_pages_buffer) == self.chunk_size:
+                page_model.objects.bulk_update(
+                    updated_pages_buffer, [self.field_name]
+                )
+                updated_pages_buffer = []
+
+        if len(updated_pages_buffer) > 0:
+            # For any remaining chunks
+            page_model.objects.bulk_update(updated_pages_buffer, [self.field_name])
+
         ContentType = apps.get_model("contenttypes", "ContentType")
         PageRevision = apps.get_model("wagtailcore", "PageRevision")
         contenttype_id = ContentType.objects.get_for_model(page_model).id
 
-        if revision_limit is not None:
+        if self.revisions_from is not None:
             revision_queryset = PageRevision.objects.filter(
-                id__in=Subquery(
-                    PageRevision.objects.filter(page_id=OuterRef("page_id"))
-                    .order_by("-created_at")
-                    .values_list("id", flat=True)[:revision_limit]
-                ),
-                page__content_type_id=contenttype_id,
+                created_at__gte=self.revisions_from, content_type_id=contenttype_id
             )
         else:
             revision_queryset = PageRevision.objects.filter(
-                page__content_type_id=contenttype_id,
+                content_type_id=contenttype_id,
             )
 
         updated_revisions_buffer = []
-        for revision in revision_queryset.iterator(chunk_size=chunk_size):
+        for revision in revision_queryset.iterator(chunk_size=self.chunk_size):
 
-            altered_raw_data = utils.apply_changes_to_raw_data(
-                raw_data=json.loads(revision.content[field_name]),
-                block_path_str=block_path_str,
-                operation=operation,
-                streamfield=getattr(page_model, field_name),
-            )
-            # - TODO add a return value to util to know if changes were made - Where would this be added?
-            # - TODO save changed only
+            altered_raw_data = json.loads(revision.content[self.field_name])
+            for operation, block_path_str in self.operations_and_block_paths:
+                altered_raw_data = utils.apply_changes_to_raw_data(
+                    raw_data=altered_raw_data,
+                    block_path_str=block_path_str,
+                    operation=operation,
+                    streamfield=getattr(page, self.field_name),
+                )
+                # - TODO add a return value to util to know if changes were made - Where would this be added?
+                # - TODO save changed only
 
-            revision.content[field_name] = json.dumps(altered_raw_data)
+            revision.content[self.field_name] = json.dumps(altered_raw_data)
             updated_revisions_buffer.append(revision)
 
-            if len(updated_revisions_buffer) == chunk_size:
+            if len(updated_revisions_buffer) == self.chunk_size:
                 PageRevision.objects.bulk_update(updated_revisions_buffer, ["content"])
                 updated_revisions_buffer = []
 
