dd7be1661943f89faf4e70f8e7c2264f7fea19df
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-07-28 15:42:20 +0530

    Added descriptions to operations

--- wagtail_streamfield_migration_toolkit/operations.py
+++ wagtail_streamfield_migration_toolkit/operations.py
@@ -7,6 +7,17 @@ class BaseBlockOperation:
 
 
 class RenameStreamChildrenOperation(BaseBlockOperation):
+    """Renames all StreamBlock children of the given type
+    
+    Note:
+        The `block_path_str` when using this operation should point to the parent StreamBlock
+        which contains the blocks to be renamed, not the block being renamed.
+
+    Attributes:
+        old_name (str): name of the child block type to be renamed 
+        new_name (str): new name to rename to
+    """
+
     def __init__(self, old_name, new_name):
         super().__init__()
         self.old_name = old_name
@@ -23,6 +34,17 @@ class RenameStreamChildrenOperation(BaseBlockOperation):
 
 
 class RenameStructChildrenOperation(BaseBlockOperation):
+    """Renames all StructBlock children of the given type
+    
+    Note:
+        The `block_path_str` when using this operation should point to the parent StructBlock
+        which contains the blocks to be renamed, not the block being renamed.
+
+    Attributes:
+        old_name (str): name of the child block type to be renamed 
+        new_name (str): new name to rename to
+    """
+
     def __init__(self, old_name, new_name):
         super().__init__()
         self.old_name = old_name
@@ -39,6 +61,16 @@ class RenameStructChildrenOperation(BaseBlockOperation):
 
 
 class RemoveStreamChildrenOperation(BaseBlockOperation):
+    """Removes all StreamBlock children of the given type
+    
+    Note:
+        The `block_path_str` when using this operation should point to the parent StreamBlock
+        which contains the blocks to be removed, not the block being removed.
+
+    Attributes:
+        name (str): name of the child block type to be removed 
+    """
+
     def __init__(self, name):
         super().__init__()
         self.name = name
@@ -52,6 +84,16 @@ class RemoveStreamChildrenOperation(BaseBlockOperation):
 
 
 class RemoveStructChildrenOperation(BaseBlockOperation):
+    """Removes all StructBlock children of the given type
+    
+    Note:
+        The `block_path_str` when using this operation should point to the parent StructBlock
+        which contains the blocks to be removed, not the block being removed.
+
+    Attributes:
+        name (str): name of the child block type to be removed 
+    """
+
     def __init__(self, name):
         super().__init__()
         self.name = name
@@ -65,6 +107,17 @@ class RemoveStructChildrenOperation(BaseBlockOperation):
 
 
 class StreamChildrenToListBlockOperation(BaseBlockOperation):
+    """Combines StreamBlock children of the given type into a new ListBlock
+    
+    Note:
+        The `block_path_str` when using this operation should point to the parent StreamBlock
+        which contains the blocks to be combined, not the child block itself.
+
+    Attributes:
+        block_name (str): name of the child block type to be combined 
+        list_block_name (str): name of the new ListBlock type
+    """
+
     def __init__(self, block_name, list_block_name):
         super().__init__()
         self.block_name = block_name
@@ -92,6 +145,17 @@ class StreamChildrenToListBlockOperation(BaseBlockOperation):
 
 
 class StreamChildrenToStreamBlockOperation(BaseBlockOperation):
+    """Combines StreamBlock children of the given types into a new StreamBlock
+    
+    Note:
+        The `block_path_str` when using this operation should point to the parent StreamBlock
+        which contains the blocks to be combined, not the child block itself.
+
+    Attributes:
+        block_names (:obj:`list` of :obj:`str`): names of the child block types to be combined 
+        stream_block_name (str): name of the new StreamBlock type
+    """
+
     def __init__(self, block_names, stream_block_name):
         super().__init__()
         self.block_names = block_names
@@ -113,6 +177,12 @@ class StreamChildrenToStreamBlockOperation(BaseBlockOperation):
 
 
 class AlterBlockValueOperation(BaseBlockOperation):
+    """Alters the value of each block to the given value
+
+    Attributes:
+        new_value : new value to change to    
+    """
+    
     def __init__(self, new_value):
         super().__init__()
         self.new_value = new_value
@@ -121,7 +191,50 @@ class AlterBlockValueOperation(BaseBlockOperation):
         return self.new_value
 
 
+# TODO what happens to IDs here
 class StreamChildrenToStructBlockOperation(BaseBlockOperation):
+    """Move each StreamBlock child of the given type inside a new StructBlock
+
+    A new StructBlock will be created as a child of the parent StreamBlock for each child block of 
+    the given type, and then that child block will be moved from the parent StreamBlocks children
+    inside the new StructBlock as a child of that StructBlock.
+
+    Example:
+        Consider the following StreamField definition::
+
+            mystream = StreamField([("char1", CharBlock()) ...], ...)
+
+        Then the stream data would look like the following::
+
+            [
+                ...
+                { "type": "char1", ... },
+                { "type": "char1", ... },
+                ...
+            ]
+
+        And if we define the operation like this::
+
+            StreamChildrenToStructBlockOperation("char1", "struct1")
+
+        Our altered stream data would look like this::
+
+            [
+                ...
+                { "type": "struct1", "value": { "char1": "..." } },
+                { "type": "struct1", "value": { "char1": "..." } },
+                ...
+            ]
+    
+    Note:
+        The `block_path_str` when using this operation should point to the parent StreamBlock
+        which contains the blocks to be combined, not the child block itself.
+
+    Attributes:
+        block_names (str): names of the child block types to be combined 
+        struct_block_name (str): name of the new StructBlock type
+    """
+
     def __init__(self, block_name, struct_block_name):
         super().__init__()
         self.block_name = block_name
