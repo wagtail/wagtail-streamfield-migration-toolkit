34cf6a438846fc15d280668330f54cf1a4fddba0
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-07-28 14:08:42 +0530

    added new operations and basic test for each

--- wagtail_streamfield_migration_toolkit/operations.py
+++ wagtail_streamfield_migration_toolkit/operations.py
@@ -62,3 +62,97 @@ class RemoveStructChildrenOperation(BaseBlockOperation):
             for child_key, child_value in block_value.items()
             if child_key != self.name
         }
+
+
+class StreamChildrenToListBlockOperation(BaseBlockOperation):
+    def __init__(self, block_name, list_block_name):
+        super().__init__()
+        self.block_name = block_name
+        self.list_block_name = list_block_name
+        self.temp_blocks = []
+
+    def apply(self, block_value):
+        mapped_block_value = []
+        for child_block in block_value:
+            if child_block["type"] == self.block_name:
+                self.temp_blocks.append(child_block)
+            else:
+                mapped_block_value.append(child_block)
+
+        self.map_temp_blocks_to_list_items()
+        new_list_block = {"type": self.list_block_name, "value": self.temp_blocks}
+        mapped_block_value.append(new_list_block)
+        return mapped_block_value
+
+    def map_temp_blocks_to_list_items(self):
+        new_temp_blocks = []
+        for block in self.temp_blocks:
+            new_temp_blocks.append({**block, "type": "item"})
+        self.temp_blocks = new_temp_blocks
+
+
+class StreamChildrenToStreamBlockOperation(BaseBlockOperation):
+    def __init__(self, block_names, stream_block_name):
+        super().__init__()
+        self.block_names = block_names
+        self.stream_block_name = stream_block_name
+
+    def apply(self, block_value):
+        mapped_block_value = []
+        stream_value = []
+
+        for child_block in block_value:
+            if child_block["type"] in self.block_names:
+                stream_value.append(child_block)
+            else:
+                mapped_block_value.append(child_block)
+
+        new_stream_block = {"type": self.stream_block_name, "value": stream_value}
+        mapped_block_value.append(new_stream_block)
+        return mapped_block_value
+
+
+class AlterBlockValueOperation(BaseBlockOperation):
+    def __init__(self, new_value):
+        super().__init__()
+        self.new_value = new_value
+
+    def apply(self, block_value):
+        return self.new_value
+
+
+class StreamChildrenToStructBlockOperation(BaseBlockOperation):
+    def __init__(self, block_name, struct_block_name):
+        super().__init__()
+        self.block_name = block_name
+        self.struct_block_name = struct_block_name
+
+    def apply(self, block_value):
+        mapped_block_value = []
+        for child_block in block_value:
+            if child_block["type"] == self.block_name:
+                mapped_block_value.append(
+                    {
+                        **child_block,
+                        "type": self.struct_block_name,
+                        "value": {self.block_name: child_block["value"]},
+                    }
+                )
+            else:
+                mapped_block_value.append(child_block)
+        return mapped_block_value
+
+
+# Note: to remember old list format
+class ListChildrenToStructBlockOperation(BaseBlockOperation):
+    def __init__(self, block_name):
+        super().__init__()
+        self.block_name = block_name
+
+    def apply(self, block_value):
+        mapped_block_value = []
+        for child_block in block_value:
+            mapped_block_value.append(
+                {**child_block, "value": {self.block_name: child_block["value"]}}
+            )
+        return mapped_block_value
--- wagtail_streamfield_migration_toolkit/test/tests/test_simple_structures.py
+++ wagtail_streamfield_migration_toolkit/test/tests/test_simple_structures.py
@@ -7,6 +7,11 @@ from wagtail_streamfield_migration_toolkit.operations import (
     RenameStructChildrenOperation,
     RemoveStreamChildrenOperation,
     RemoveStructChildrenOperation,
+    StreamChildrenToListBlockOperation,
+    StreamChildrenToStreamBlockOperation,
+    StreamChildrenToStructBlockOperation,
+    ListChildrenToStructBlockOperation,
+    AlterBlockValueOperation,
 )
 
 
@@ -66,11 +71,133 @@ class FieldChildBlockTest(TestCase):
         self.assertEqual(altered_raw_data[0]["type"], "char2")
         self.assertEqual(altered_raw_data[1]["type"], "char2")
 
-    def test_to_listblock(self):
-        pass
+    def test_combine_to_listblock(self):
+        """Combine all `char1` blocks into a new ListBlock named `list1`
 
-    def test_to_streamblock(self):
-        pass
+        Check whether other blocks are intact.
+        Check whether no `char1` blocks are present among the stream children.
+        Check whether a new `list1` block has been added to the stream children.
+        Check whether the new `list1` block has the data from the `char1` blocks.
+        """
+        altered_raw_data = apply_changes_to_raw_data(
+            self.raw_data,
+            "",
+            StreamChildrenToListBlockOperation(
+                block_name="char1", list_block_name="list1"
+            ),
+            streamfield=models.SampleModel.content,
+        )
+
+        self.assertEqual(len(altered_raw_data), 3)
+        self.assertEqual(altered_raw_data[0]["type"], "char2")
+        self.assertEqual(altered_raw_data[1]["type"], "char2")
+
+        self.assertEqual(altered_raw_data[2]["type"], "list1")
+        self.assertEqual(len(altered_raw_data[2]["value"]), 2)
+        self.assertEqual(altered_raw_data[2]["value"][0]["type"], "item")
+        self.assertEqual(altered_raw_data[2]["value"][0]["value"], "Char Block 1")
+        self.assertEqual(altered_raw_data[2]["value"][1]["type"], "item")
+        self.assertEqual(altered_raw_data[2]["value"][1]["value"], "Char Block 1")
+
+    def test_combine_single_type_to_streamblock(self):
+        """Combine all `char1` blocks as children of a new StreamBlock named `stream1`
+
+        Check whether other blocks are intact.
+        Check whether no `char1` blocks are present among the (top) stream children.
+        Check whether a new `stream1` block has been added to the (top) stream children.
+        Check whether the new `stream1` block has the `char1` blocks as children.
+        """
+
+        altered_raw_data = apply_changes_to_raw_data(
+            self.raw_data,
+            "",
+            StreamChildrenToStreamBlockOperation(
+                block_names=["char1"], stream_block_name="stream1"
+            ),
+            streamfield=models.SampleModel.content,
+        )
+
+        self.assertEqual(len(altered_raw_data), 3)
+        self.assertEqual(altered_raw_data[0]["type"], "char2")
+        self.assertEqual(altered_raw_data[1]["type"], "char2")
+        self.assertEqual(altered_raw_data[2]["type"], "stream1")
+
+        self.assertEqual(len(altered_raw_data[2]["value"]), 2)
+        self.assertEqual(altered_raw_data[2]["value"][0]["type"], "char1")
+        self.assertEqual(altered_raw_data[2]["value"][1]["type"], "char1")
+
+    def test_combine_multiple_types_to_streamblock(self):
+        """Combine all `char1` and `char2` blocks as children of a new StreamBlock named `stream1`
+
+        Check whether no `char1` or `char2` blocks are present among the (top) stream children.
+        Check whether a new `stream1` block has been added to the (top) stream children.
+        Check whether the new `stream1` block has the `char1` and `char2` blocks as children.
+
+        Note:
+            We only have `char1` and `char2` blocks in our existing data. (TODO) We may need to add
+            more blocks to test whether other blocks are intact.
+        """
+
+        altered_raw_data = apply_changes_to_raw_data(
+            self.raw_data,
+            "",
+            StreamChildrenToStreamBlockOperation(
+                block_names=["char1", "char2"], stream_block_name="stream1"
+            ),
+            streamfield=models.SampleModel.content,
+        )
+
+        self.assertEqual(len(altered_raw_data), 1)
+        self.assertEqual(altered_raw_data[0]["type"], "stream1")
+
+        self.assertEqual(len(altered_raw_data[0]["value"]), 4)
+        self.assertEqual(altered_raw_data[0]["value"][0]["type"], "char1")
+        self.assertEqual(altered_raw_data[0]["value"][1]["type"], "char2")
+        self.assertEqual(altered_raw_data[0]["value"][2]["type"], "char1")
+        self.assertEqual(altered_raw_data[0]["value"][3]["type"], "char2")
+
+    def test_to_structblock(self):
+        """Move each `char1` block inside a new StructBlock named `struct1`
+
+        Check whether each `char1` block has been replaced with a `struct1` block in the stream
+        children.
+        Check whether other blocks are intact.
+        Check whether each `struct1` block has a `char1` child.
+        """
+
+        altered_raw_data = apply_changes_to_raw_data(
+            self.raw_data,
+            "",
+            StreamChildrenToStructBlockOperation("char1", "struct1"),
+            streamfield=models.SampleModel.content,
+        )
+
+        self.assertEqual(altered_raw_data[0]["type"], "struct1")
+        self.assertEqual(altered_raw_data[1]["type"], "char2")
+        self.assertEqual(altered_raw_data[2]["type"], "struct1")
+        self.assertEqual(altered_raw_data[3]["type"], "char2")
+
+        self.assertIn("char1", altered_raw_data[0]["value"])
+        self.assertIn("char1", altered_raw_data[2]["value"])
+
+    def test_alter_value(self):
+        """Change the value of each `char1` block to `foo`
+
+        Check whether the value of each `char1` block has changed to `foo`.
+        Check whether the values of other blocks are intact.
+        """
+
+        altered_raw_data = apply_changes_to_raw_data(
+            self.raw_data,
+            "char1",
+            AlterBlockValueOperation(new_value="foo"),
+            streamfield=models.SampleModel.content,
+        )
+
+        self.assertEqual(altered_raw_data[0]["value"], "foo")
+        self.assertEqual(altered_raw_data[1]["value"], "Char Block 2")
+        self.assertEqual(altered_raw_data[2]["value"], "foo")
+        self.assertEqual(altered_raw_data[3]["value"], "Char Block 2")
 
 
 class FieldStructChildBlockTest(TestCase):
