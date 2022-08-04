bdedc211e1038476d70f4e8ab4c3ba3c53468f6f
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-08-04 14:48:20 +0530

    finding parent type, hence operation class

--- wagtail_streamfield_migration_toolkit/management/commands/streamdatamigration.py
+++ wagtail_streamfield_migration_toolkit/management/commands/streamdatamigration.py
@@ -6,11 +6,14 @@ from django.db.migrations.autodetector import MigrationAutodetector
 from django.db.migrations.state import ProjectState
 from django.db.migrations.writer import MigrationWriter
 from django.apps import apps
+from wagtail.blocks import StreamBlock, StructBlock
 
 from wagtail_streamfield_migration_toolkit.migrate_operation import MigrateStreamData
 from wagtail_streamfield_migration_toolkit.operations import (
     RemoveStreamChildrenOperation,
     RenameStreamChildrenOperation,
+    RemoveStructChildrenOperation,
+    RenameStructChildrenOperation,
 )
 
 
@@ -46,6 +49,10 @@ class Command(BaseCommand):
                 "You must supply path to StreamField as '<app_name>.<model_name>.<field_name>'"
             )
 
+        loader = MigrationLoader(connection=connection)
+        loader.build_graph()
+        self.project_state = loader.project_state()
+
         # TODO do we have to add a check for flags being mutually exclusive?
         if self.is_rename:
             migration_operation = self.make_rename_operation(*options["rename"])
@@ -62,10 +69,8 @@ class Command(BaseCommand):
         if migration_operation:
             migration.operations = [migration_operation]
 
-        loader = MigrationLoader(connection=connection)
-        loader.build_graph()
         autodetector = MigrationAutodetector(
-            loader.project_state(), ProjectState.from_apps(apps)
+            self.project_state, ProjectState.from_apps(apps)
         )
         changes = {self.app_label: [migration]}
         changes = autodetector.arrange_for_graph(
@@ -87,14 +92,23 @@ class Command(BaseCommand):
         try:
             if len(args) == 2:
                 old_name, new_name = args
-                block_path = ""
+                block_path_str = ""
             else:
-                block_path, old_name, new_name = args
+                block_path_str, old_name, new_name = args
         except ValueError:
             raise CommandError(
                 "Rename operation needs the following arguments: <block_path> <old_name> <new_name>"
             )
-        # TODO go through structure and figure out the relevant parent type and operation
+
+        self.block_path_str = block_path_str
+        block_def = self.get_block_def()
+
+        if isinstance(block_def, StreamBlock):
+            data_operation = RenameStreamChildrenOperation
+        elif isinstance(block_def, StructBlock):
+            data_operation = RenameStructChildrenOperation
+        else:
+            raise CommandError("Invalid Block Structure")
 
         if not self.migration_name:
             self.migration_name = "rename_{}_to_{}".format(old_name, new_name)
@@ -102,7 +116,7 @@ class Command(BaseCommand):
             self.app_label,
             self.model_label,
             self.field_label,
-            [(RenameStreamChildrenOperation(old_name, new_name), block_path)],
+            [(data_operation(old_name, new_name), block_path_str)],
         )
         return migration_operation
 
@@ -111,14 +125,23 @@ class Command(BaseCommand):
         try:
             if len(args) == 1:
                 block_name = args[0]
-                block_path = ""
+                block_path_str = ""
             else:
-                block_path, block_name = args
+                block_path_str, block_name = args
         except ValueError:
             raise CommandError(
                 "Remove operation needs the following arguments: <block_path> <block_name>"
             )
-        # TODO go through structure and figure out the relevant parent type and operations
+
+        self.block_path_str = block_path_str
+        block_def = self.get_block_def()
+
+        if isinstance(block_def, StreamBlock):
+            data_operation = RemoveStreamChildrenOperation
+        elif isinstance(block_def, StructBlock):
+            data_operation = RemoveStructChildrenOperation
+        else:
+            raise CommandError("Invalid Block Structure")
 
         if not self.migration_name:
             self.migration_name = "remove_block_{}".format(block_name)
@@ -126,6 +149,26 @@ class Command(BaseCommand):
             self.app_label,
             self.model_label,
             self.field_label,
-            [(RemoveStreamChildrenOperation(block_name), block_path)],
+            [(data_operation(block_name), block_path_str)],
         )
         return migration_operation
+
+    def get_block_def(self):
+
+        if self.block_path_str == "":
+            block_path = []
+        else:
+            block_path = self.block_path_str.split(".")
+        model = self.project_state.apps.get_model(self.app_label, self.model_label)
+        stream_field = getattr(model, self.field_label)
+        block_def = stream_field.field.stream_block
+        while len(block_path) > 0:
+            try:
+                block_def = block_def.child_blocks[block_path[0]]
+            except AttributeError:
+                block_def = block_def.child_block
+            except KeyError:
+                raise CommandError("Invalid Block Path")
+            block_path = block_path[1:]
+
+        return block_def
