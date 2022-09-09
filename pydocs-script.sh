034fe65a5f7c3493b6aff90a6b5cad9b58735f06
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-09-09 14:49:47 +0530

    makefile instead of sh script

--- pydocs-script.sh
+++
@@ -1,5 +0,0 @@
-pydoc-markdown -I . \
-    -m wagtail_streamfield_migration_toolkit.migrate_operation \
-    -m wagtail_streamfield_migration_toolkit.operations \
-    -m wagtail_streamfield_migration_toolkit.utils \
-    --render-toc > docs/REFERENCE.md
