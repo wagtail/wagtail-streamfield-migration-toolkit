55d86752d4166876e2c32416591cbc4bf1393488
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-09-30 14:40:47 +0530

    assertBlocksAlmostEqual for testing block compare

--- wagtail_streamfield_migration_toolkit/test/tests/test_block_def_compare.py
+++ wagtail_streamfield_migration_toolkit/test/tests/test_block_def_compare.py
@@ -33,41 +33,57 @@ class DiffThreeFieldStruct(StructBlock):
     my_boolean = BooleanBlock()
 
 
-class TestStructBlocks(TestCase):
+class BlockComparerTestCase(TestCase):
+    def assertBlocksAlmostEqual(
+        self,
+        comparer,
+        old_def,
+        old_name,
+        new_def,
+        new_name,
+        arg_similarity=1,
+        child_similarity=1,
+        name_similarity=1,
+    ):
+        comparison_score = comparer.compare(
+            old_def=old_def, old_name=old_name, new_def=new_def, new_name=new_name
+        )
+        expected_score = (
+            comparer.arg_weight * arg_similarity
+            + comparer.child_weight * child_similarity
+            + comparer.name_weight * name_similarity
+        ) / (comparer.arg_weight + comparer.child_weight + comparer.name_weight)
+        self.assertAlmostEqual(comparison_score, expected_score, delta=0.01)
+
+
+class TestStructBlocks(BlockComparerTestCase):
     def test(self):
         # These will need to change if we change the logic for comparison
 
-        self.assertAlmostEqual(
-            StructBlockDefComparer.compare(
-                old_def=FourFieldStruct(),
-                old_name="foo",
-                new_def=ThreeFieldStruct(),
-                new_name="foo",
-            ),
-            3 / 4,
-            delta=0.01,
+        self.assertBlocksAlmostEqual(
+            comparer=StructBlockDefComparer,
+            old_def=FourFieldStruct(),
+            old_name="foo",
+            new_def=ThreeFieldStruct(),
+            new_name="foo",
+            child_similarity=3 / 4,
         )
 
-        self.assertAlmostEqual(
-            StructBlockDefComparer.compare(
-                old_def=ThreeFieldStruct(),
-                old_name="foo",
-                new_def=FourFieldStruct(),
-                new_name="foo",
-            ),
-            3 / 3,
-            delta=0.01,
+        self.assertBlocksAlmostEqual(
+            comparer=StructBlockDefComparer,
+            old_def=ThreeFieldStruct(),
+            old_name="foo",
+            new_def=FourFieldStruct(),
+            new_name="foo",
         )
 
-        self.assertAlmostEqual(
-            StructBlockDefComparer.compare(
-                old_def=ThreeFieldStruct(),
-                old_name="foo",
-                new_def=DiffThreeFieldStruct(),
-                new_name="foo",
-            ),
-            0 / 3,
-            delta=0.01,
+        self.assertBlocksAlmostEqual(
+            comparer=StructBlockDefComparer,
+            old_def=ThreeFieldStruct(),
+            old_name="foo",
+            new_def=DiffThreeFieldStruct(),
+            new_name="foo",
+            child_similarity=0,
         )
 
 
@@ -87,39 +103,32 @@ class DiffThreeFieldStream(StreamBlock):
     my_boolean = BooleanBlock()
 
 
-class TestStreamBlocks(TestCase):
+class TestStreamBlocks(BlockComparerTestCase):
     def test(self):
         # These will need to change if we change the logic for comparison
 
-        self.assertAlmostEqual(
-            StreamBlockDefComparer.compare(
-                old_def=FourFieldStream(),
-                old_name="foo",
-                new_def=ThreeFieldStream(),
-                new_name="foo",
-            ),
-            3 / 4,
-            delta=0.01,
+        self.assertBlocksAlmostEqual(
+            comparer=StreamBlockDefComparer,
+            old_def=FourFieldStream(),
+            old_name="foo",
+            new_def=ThreeFieldStream(),
+            new_name="foo",
+            child_similarity=3 / 4,
         )
 
-        self.assertAlmostEqual(
-            StreamBlockDefComparer.compare(
-                old_def=ThreeFieldStream(),
-                old_name="foo",
-                new_def=FourFieldStream(),
-                new_name="foo",
-            ),
-            3 / 3,
-            delta=0.01,
+        self.assertBlocksAlmostEqual(
+            comparer=StreamBlockDefComparer,
+            old_def=ThreeFieldStream(),
+            old_name="foo",
+            new_def=FourFieldStream(),
+            new_name="foo",
         )
 
-        self.assertAlmostEqual(
-            StreamBlockDefComparer.compare(
-                old_def=ThreeFieldStream(),
-                old_name="foo",
-                new_def=DiffThreeFieldStream(),
-                new_name="foo",
-            ),
-            0 / 3,
-            delta=0.01,
+        self.assertBlocksAlmostEqual(
+            comparer=StreamBlockDefComparer,
+            old_def=ThreeFieldStream(),
+            old_name="foo",
+            new_def=DiffThreeFieldStream(),
+            new_name="foo",
+            child_similarity=0,
         )
